ca856baa6e31301622351727c5ce6a66f7ae9b76
Blugon <71944969+Blugon0921@users.noreply.github.com>
2022-03-30 02:00:51 +0900

    ✨ Renewal

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+function discordHoverEvent() {
+    // document.getElementsByClassName("discord")[0].textContent = "블루곤#0001"
+}
+function discordLeaveEvent() {
+    // document.getElementsByClassName("discord")[0].textContent = ""
+    // let i = document.createElement('i')
+    // i.className = "fa-brands fa-discord"
+    // // i.textContent += " Discord"
+    // document.getElementsByClassName("discord")[0].appendChild(i)
+    // document.getElementsByClassName("discord")[0].textContent += " Discord"
+}
